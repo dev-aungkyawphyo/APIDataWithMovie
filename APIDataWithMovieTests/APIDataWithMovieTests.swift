6982a8e78a196270945876d223e2c77c70dfefc2
Aung Kyaw Phyo <Dev.aungkyawphyo@gmail.com>
2025-01-20 00:28:07 +0630

    Initial Commit

---
+++ APIDataWithMovieTests/APIDataWithMovieTests.swift
@@ -0,0 +1,17 @@
+//
+//  APIDataWithMovieTests.swift
+//  APIDataWithMovieTests
+//
+//  Created by Aung Kyaw Phyo on 20/01/2025.
+//
+
+import Testing
+@testable import APIDataWithMovie
+
+struct APIDataWithMovieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
